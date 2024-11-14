0f221b96302f62c422fe74dcb857345be646c9a6
Angello2525 <miguelangeltorresaguirre1@gmail.com>
2024-11-14 07:53:28 -0500

    Aca por fin el footer :D

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Conocenos from './pages/Conocenos'
 import GuiasArt from './pages/GuiasArt'
 import Contacto from './pages/Contacto'
 import Productos from './pages/Productos'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/productos" element={<Productos />} />
       </Routes>
+      <Footer />
     </>
   )
 }
